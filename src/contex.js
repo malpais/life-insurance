0288c0f8bba0edc6f0e3434b45898c52ba378c66
tangentdigitalagency <david@tangentdigitalagency.com>
2021-06-29 13:28:52 -0500

    Fixed minor erorsa and bugs

--- src/contex.js
+++ src/contex.js
@@ -32,7 +32,7 @@ class ContextApiProvider extends Component {
 		formData.append('last_name', this.state.last_name);
 		formData.append('email_address', this.state.email_address);
 		formData.append('phone_cell', this.state.phone_cell);
-		formData.append('landing_page', 'life.quotehound.com');
+		formData.append('landing_Page', 'life.quotehound.com');
 		formData.append('user_agent', navigator.userAgent);
 		try {
 			let insurance = await axios.post('https://quotehound.leadspediatrack.com/post.do', formData);
